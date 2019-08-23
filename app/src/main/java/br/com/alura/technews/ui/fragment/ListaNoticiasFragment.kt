f86122e28107e3b9b32cc2cef343287546dda2c5
Josue <josue10599@gmail.com>
2019-08-23 17:21:33 -0300

    Lista Noticias migrado para fragment

---
+++ app/src/main/java/br/com/alura/technews/ui/fragment/ListaNoticiasFragment.kt
@@ -0,0 +1,68 @@
+package br.com.alura.technews.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.alura.technews.R
+import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.ui.fragment.extensions.mostraErro
+import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
+import br.com.alura.technews.ui.viewModel.ListaNoticiasViewModel
+import kotlinx.android.synthetic.main.fragment_lista_noticias.*
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
+
+class ListaNoticiasFragment : Fragment() {
+    private val listaNoticiasViewModels: ListaNoticiasViewModel by viewModel()
+    private val adapter by inject<ListaNoticiasAdapter>()
+    var abreVisualizadorNoticia: (noticia: Noticia) -> Unit = {}
+    var abreFormularioModoCriacao: () -> Unit = {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        buscaNoticias()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lista_noticias, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configuraRecyclerView()
+        configuraFabAdicionaNoticia()
+    }
+
+    private fun configuraFabAdicionaNoticia() {
+        fragment_lista_noticias_fab_salva_noticia.setOnClickListener { abreFormularioModoCriacao }
+    }
+
+    private fun configuraRecyclerView() {
+        val divisor = DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+        fragment_lista_noticias_recyclerview.addItemDecoration(divisor)
+        fragment_lista_noticias_recyclerview.adapter = adapter
+        configuraAdapter()
+    }
+
+    private fun configuraAdapter() {
+        adapter.quandoItemClicado = abreVisualizadorNoticia
+    }
+
+    private fun buscaNoticias() {
+        listaNoticiasViewModels.buscaTodos().observe(this, Observer { resource ->
+            resource.dado?.let { adapter.atualiza(it) }
+            resource.erro?.let { mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS) }
+        })
+    }
+}
